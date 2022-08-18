0c617b8fce6a74d0b1441a171396846e9bd624fc
Gabriel <gabrielgomesportela@gmail.com>
2022-08-18 10:56:26 -0300

    implemented the distribution of cards

--- js/script.js
+++ js/script.js
@@ -1,14 +1,66 @@
-let number;
-
-numberOfCards();
+let quantity;
+const imageCard = [
+    './img/bobrossparrot.gif',
+    './img/bobrossparrot.gif',
+    './img/explodyparrot.gif',
+    './img/explodyparrot.gif',
+    './img/fiestaparrot.gif',
+    './img/fiestaparrot.gif',
+    './img/metalparrot.gif',
+    './img/metalparrot.gif',
+    './img/revertitparrot.gif',
+    './img/revertitparrot.gif',
+    './img/tripletsparrot.gif',
+    './img/tripletsparrot.gif',
+    './img/unicornparrot.gif',
+    './img/unicornparrot.gif'
+];
+let newImageCard = [];
 
 function numberOfCards() {
-    const number = parseInt(prompt("Com quantas cartas você deseja jogar? \nInsira um número par, de 4 a 14." ));
-    console.log(number);
-    if ((number%2) == 0) {
+    quantity = parseInt(prompt("Com quantas cartas você deseja jogar? \nInsira um número par, de 4 a 14." ));
+    if ((quantity%2) == 0 && quantity>=4 && quantity<=14) {
         dealTheCards();
     } else {
         alert("Você precisa digitar um número par, de 4 e 14!");
         numberOfCards();
+    }
+}
+
+numberOfCards();
+
+function shuffle() {
+    newImageCard.sort(indicator);
+}
+
+
+function indicator() {
+    return Math.random() - 0.5;
+}
+
+function dealTheCards() {
+    const card = document.querySelector('ul');
+
+    newImageCard = imageCard.slice(0, quantity);
+
+    shuffle();
+
+    let index = 0;
+    
+    while(quantity > index) {
+        let cardDistribution = `
+        <li class="card">
+            <div class="back-face face">
+                <img src="./img/back.png">
+            </div>
+            <div class="front-face face">
+               <img src="${newImageCard[index]}">
+            </div>
+        </li>
+        `;
+
+        card.innerHTML = card.innerHTML + cardDistribution;
+
+        index++;
     }
 }
