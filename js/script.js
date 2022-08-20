0acef15593c6e9c8422458118b985488a1421ea8
Gabriel <gabrielgomesportela@gmail.com>
2022-08-19 22:57:31 -0300

    flip cards feature implemented

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
 let quantity;
-const imageCard = [
-    './img/bobrossparrot.gif',
-    './img/bobrossparrot.gif',
-    './img/explodyparrot.gif',
-    './img/explodyparrot.gif',
-    './img/fiestaparrot.gif',
-    './img/fiestaparrot.gif',
-    './img/metalparrot.gif',
-    './img/metalparrot.gif',
-    './img/revertitparrot.gif',
-    './img/revertitparrot.gif',
-    './img/tripletsparrot.gif',
-    './img/tripletsparrot.gif',
-    './img/unicornparrot.gif',
-    './img/unicornparrot.gif'
+let imageCard = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot',
 ];
 let newImageCard = [];
+let moves = 0;
+let card1 = '';
+let card2 = '';
+let scoreboard = 0;
 
 function numberOfCards() {
-    quantity = parseInt(prompt("Com quantas cartas você deseja jogar? \nInsira um número par, de 4 a 14." ));
-    if ((quantity%2) == 0 && quantity>=4 && quantity<=14) {
+    quantity = parseInt(prompt("Com quantas cartas você deseja jogar? \nInsira um número par, de 4 a 14."));
+    if ((quantity % 2) == 0 && quantity >= 4 && quantity <= 14) {
         dealTheCards();
     } else {
         alert("Você precisa digitar um número par, de 4 e 14!");
@@ -29,32 +26,25 @@ function numberOfCards() {
 
 numberOfCards();
 
-function shuffle() {
-    newImageCard.sort(indicator);
-}
-
-
-function indicator() {
-    return Math.random() - 0.5;
-}
-
 function dealTheCards() {
     const card = document.querySelector('ul');
 
-    newImageCard = imageCard.slice(0, quantity);
+    const elements = quantity / 2;
 
-    shuffle();
+    newImageCard = [...imageCard.slice(0, elements), ...imageCard.slice(0, elements)];
+
+    newImageCard = newImageCard.sort(indicator);
 
     let index = 0;
-    
-    while(quantity > index) {
+
+    while (quantity > index) {
         let cardDistribution = `
-        <li class="card">
+        <li class="card" onclick="flipCard(this)" id="${newImageCard[index]}">
             <div class="back-face face">
                 <img src="./img/back.png">
             </div>
             <div class="front-face face">
-               <img src="${newImageCard[index]}">
+               <img src="./img/${newImageCard[index]}.gif">
             </div>
         </li>
         `;
@@ -62,5 +52,31 @@ function dealTheCards() {
         card.innerHTML = card.innerHTML + cardDistribution;
 
         index++;
+    }
+}
+
+function indicator() {
+    return Math.random() - 0.5;
+}
+
+function flipCard(flippedCard) {
+    const selectedCard = document.querySelector('.cards .flip');
+    const classe = flippedCard.getAttribute('class');
+    if (classe == 'card flip') {
+        return;
+    } else {
+        if (card1 == '') {
+            flippedCard.classList.add('flip');
+            card1 = flippedCard;
+            moves++;
+            return;
+        } else if (card2 == '') {
+            flippedCard.classList.add('flip');
+            card2 = flippedCard;
+            moves++;
+        }
+    }
+    if (card1 !== null && card2 !== null) {
+        setTimeout(compareCards, 1000);
     }
 }
